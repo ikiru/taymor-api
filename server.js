cc396bc8fc2b194f1c819523dfd68e3f4014c8b4
jeff@rhinofleettracking.com <jeff.a.winkler@gmail.com>
2018-02-23 22:39:39 -0600

    added some graphql

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
-const env = 'development'
+const dev = process.env.NODE_ENV === 'development'
 const config = require('./knexfile.js')[env]
 const knex = require('knex')(config)
 
-
 const express = require('express')
 const bodyParser = require('body-parser')
+const graphqlHTTP = require('express-graphql')
+const schema = require('./graphql/schema')
+
 const port = process.env.Port || 8000
-
-
 const app = express()
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: dev,
+}))
+
+app.use('/', (req, res) => {
+  res.json('Go to /graphql to test your queries and mutations!');
+})
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
