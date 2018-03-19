e03826693ed548fa1119877a48f4bf30cfeb4068
jeff@rhinofleettracking.com <jeff.a.winkler@gmail.com>
2018-03-19 08:05:09 -0500

    continue the removal of knex.js

---
+++ db.js
@@ -0,0 +1,49 @@
+const mssql = require("mssql");
+
+const dbProdConfig = {
+  // user: "",
+  // password: "",
+  // server: "",
+  // database: "",
+  // port: ""
+ };
+
+const dbDevConfig = {
+  user: "Ikiru",
+  password: "Pepper00~~!!",
+  server: "betteroff.cxc47tthyp3m.us-east-2.rds.amazonaws.com",
+  database: "taymor_development",
+  // options: {
+  //   encrypt: true,
+  //   port: "5432"
+  // }
+};
+
+// export const cpool = new mssql.connect(dbProdConfig)
+// export const cpool = new mssql.connect(dbDevConfig)
+export const cpool = process.env.NODE_ENV == 'dev' ? new mssql.connect(dbDevConfig) : new mssql.connect(dbProdConfig)
+
+export async function token(authCode, token) {
+  const result = await cpool
+    .then(pool => {
+      return pool
+        .request()
+        .input("authCode", mssql.VarChar(50), authCode)
+        .input("token", mssql.VarChar(50), token)
+        .query("exec gps.dbo.v3_Token_verify @authCode, @token");
+    })
+    .then(result => {
+      if (result.recordsets[0][0].resultCode == 1) {
+        return result.recordsets[0][0].token
+      }
+      else {
+        return null
+      }
+
+    })
+    .catch(err => {
+      console.log(err);
+    });
+
+  return result
+}
--- orm/bookshelf.js
+++
@@ -1,5 +0,0 @@
-var knex = require('./knex')(dbConfig);
-
-
-module.exports = require('bookshelf')(knex);
-
