57dd672455f8655eb0a0fa5dd62e9925b1faec85
jeff@rhinofleettracking.com <jeff.a.winkler@gmail.com>
2018-03-20 07:52:50 -0500

    started working on queries

--- graphql/queries/queries.js
+++ graphql/queries/queries.js
@@ -6,3 +6,29 @@ const {} = require("./schema.js");
 // import { } from "./enums";
 // const { } = require("../enums/enums");
 
+const QueryRoot = new GraphQLObjectType({
+name: "Queries",
+descrition: "All the queries for the schema",
+fields: {
+  Colors: {
+    name: 'color',
+    type: new GraphQLList(AlertReport),
+    description: "",
+    args: {
+      clientId: {
+        type: GraphQLString,
+        description: 'Client identifier'
+      }
+    },
+    resolve: function(source, args, context, info) {
+      return cpool.then(pool => {
+        return pool.request().input('clientId', args.clientId).execute('')
+      }).then(result => {
+        return result.recordsets[0]
+      }).catch(err => {
+        console.log(err)
+      })
+    }
+  },
+
+})
